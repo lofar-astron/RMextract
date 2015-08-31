4788d3e254728c03fad814dd8a98da25dfcd7202
maaijke <m.mevius@rug.nl>
2015-08-31 15:48:40 +0200

    removed small bug in getRM.py

--- examples/example_get_RM_3C196.py
+++ examples/example_get_RM_3C196.py
@@ -1,12 +1,13 @@
 import RMextract.getRM as gt
 import pyrap.tables as tab
 from pylab import *
-a=tab.taql('calc MJD("2014/03/01/00:00:00")')[0]*3600*24
-b=tab.taql('calc MJD("2014/03/20/00:00:00")')[0]*3600*24
+a=tab.taql('calc MJD("2015/07/22/00:00:00")')[0]*3600*24
+b=tab.taql('calc MJD("2015/07/22/23:59:00")')[0]*3600*24
 
 statpos=gt.PosTools.posCS002
 pointing=array([ 2.15374123,  0.8415521 ]) #3C196
-tec=gt.getRM(ionexPath='./IONEXdata/',earth_rot=1,ha_limit=0.25*np.pi,radec=pointing,timestep=1800, timerange = [a, b],stat_positions=[statpos,])
+tec=gt.getRM(ionexPath='./IONEXdata/',earth_rot=0,ha_limit=0.5*np.pi,radec=pointing,timestep=450, timerange = [a, b],stat_positions=[statpos,],server="ftp://gnss.oma.be/gnss/products/IONEX/",prefix="ROBR")
+tec2=gt.getRM(ionexPath='./IONEXdata/',earth_rot=0,ha_limit=0.5*np.pi,radec=pointing,timestep=450, timerange = [a, b],stat_positions=[statpos,])
 times=tec['times']
 flags=tec['flags']['st1']
 timess=[tm/(3600*24.) for tm in times]
@@ -22,5 +23,5 @@ maskeddata=np.ma.array(tec['RM']['st1'],mask=np.logical_not(flags))
 plot_date(mydatetimes,maskeddata,'-')
 plt.gcf().autofmt_xdate()
 ylabel("RM (rad/m^2)")
-title("RM variation winter 2012/2013")
+title("RM variation direction 3C196 2015/07/22")
 show()
