0aa7a1096d68e8233a2ed8a2d7ce9cc89a2a4047
maaijke <m.mevius@rug.nl>
2015-10-13 11:08:17 +0200

    bug fix

--- RMextract/getIONEX.py
+++ RMextract/getIONEX.py
@@ -286,7 +286,7 @@ retval    O  The return code of the command.  Note that if the process times out
     """
     #first check if command is exectuable:
     if not cmd_exists(command):
-	    raise RuntimeError, "Could not run '%s'."%command,"Please check your PATH"
+	    raise RuntimeError, "Could not run '%s'.Please check your PATH"%command,
     pid = os.fork()
     #print "my pid",pid
     if(pid == 0):
