e875faa3e4d3910a0635288e6eb9f9fc0e01c85d
maaijke <maaijke_2@hotmail.com>
2016-07-29 08:24:02 +0200

    set iri optional in build

--- setup.py
+++ setup.py
@@ -3,18 +3,23 @@ from numpy.distutils.core import setup
 from numpy.distutils.core import Extension as Extension2
 import sys
 
-packages=['RMextract','EMM','pyiri']
+packages=['RMextract','EMM']
 scripts = ['RMextract/URL_download.py']
+ext=[Extension2('_EMM_Model', ['EMM/EMM_Model.cc','EMM/GeomagnetismLibrary.c','EMM/EMM_Model_wrap.cc'],extra_compile_args=['-Wno-format-security'])]
+
 if "--add-lofar-utils" in sys.argv:
     packages.append("RMextract/LOFAR_TOOLS")
     scripts.append("RMextract/LOFAR_TOOLS/createRMParmdb")
     sys.argv.remove("--add-lofar-utils")
+if "--add-iri" in sys.argv:
+    packages.append("pyiri")
+    ext.append(Extension2('_iri',sources=['pyiri/iri.pyf','pyiri/cira.for',  'pyiri/igrf.for',  'pyiri/iridreg.for',  'pyiri/iriflip.for',  'pyiri/irifun.for',  'pyiri/irisub.for',  'pyiri/iritec.for',  'pyiri/iriget.for']))
+    sys.argv.remove("--add-iri")
 
 
 setup(name='RMextract',
 version='0.1',
-      ext_modules=[Extension2('_EMM_Model', ['EMM/EMM_Model.cc','EMM/GeomagnetismLibrary.c','EMM/EMM_Model_wrap.cc'],extra_compile_args=['-Wno-format-security']),
-                   Extension2('_iri',sources=['pyiri/iri.pyf','pyiri/cira.for',  'pyiri/igrf.for',  'pyiri/iridreg.for',  'pyiri/iriflip.for',  'pyiri/irifun.for',  'pyiri/irisub.for',  'pyiri/iritec.for',  'pyiri/iriget.for'])],
+      ext_modules=ext,
       packages= packages,
       package_data={'EMM':['*COF'],
                     'pyiri':['*dat','*asc']},
