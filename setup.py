5736a64e010da29cc62426ad14a77e0cff89cfa7
Marcel Loose <loose@astron.nl>
2021-07-01 13:00:44 +0200

    Fix broken `pip install` from source distribution
    When `pip` installing using a source distribution, the build phase fails
    due to missing `fortranobject.c` and `fortranobject.h` files. I thinks this
    is a bug in Numpy's distuils. The work-around is to override the `build_src`
    class in Numpy's distutils and handle copying those files in there.

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import os
 from setuptools import find_packages
 import numpy
 from numpy.distutils.core import setup, Extension
+from numpy.distutils.command.build_src import build_src as np_build_src
+from numpy.distutils.misc_util import has_f_sources
 
 
-# Function read() was copied from setup.py in Pip package.
 def read(rel_path):
+    """Function read() was copied from setup.py in Pip package."""
     here = os.path.abspath(os.path.dirname(__file__))
     # intentionally *not* adding an encoding option to open, See:
     #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
@@ -13,8 +15,35 @@ def read(rel_path):
         return fp.read()
 
 
-packages = find_packages(exclude=["RMextract.LOFAR_TOOLS"])
+class build_src(np_build_src):
+    """
+    Override `build_src` class in `numpy.distutils.command.build_src`.
+    This is necessary, because, when using `pip` (i.e. `setuptools`) to install from a source distribution,
+    the files `fortranobject.h` and `fortranobject.` are not copied to the build source tree. However, these
+    files are needed to build extension modules from Fortran sources. So we need to take care of that here,
+    by overriding the `run` method in `build_src`.
+    """
 
+    def run(self):
+        # Assume that `fortranobject.h` and `fortranobject.c` are in the directory `numpy/f2py/src`.
+        srcdir = os.path.join(os.path.dirname(numpy.__file__), 'f2py', 'src')
+        for ext in self.extensions:
+            # If the extension contains Fortran sources, we need to copy `fortranobject.h` and `fortranobject.c`
+            # from the f2py source directory to the extension's build directory.
+            if has_f_sources(ext.sources):
+                # The build source tree must have the same structure as the source tree. So the name of the destination
+                # directory is the concatenation of the name of the build source tree and the name of the directory
+                # containing the Fortran source files. Here we assume that all the Fortran source files are in the same
+                # directory, so we simply take the first one in the list to determine the source directory.
+                destdir = os.path.join(self.build_src, os.path.dirname(ext.sources[0]))
+                os.makedirs(destdir, exist_ok=True)
+                for fn in ('fortranobject.h', 'fortranobject.c'):
+                    self.copy_file(os.path.join(srcdir, fn), os.path.join(destdir, fn))
+        # Call the `run()` method of the parent
+        super().run()
+
+
+packages = find_packages(exclude=["RMextract.LOFAR_TOOLS"])
 ext_modules = []
 scripts = []
 
@@ -95,6 +124,7 @@ setup(
         "Topic :: Scientific/Engineering :: Astronomy",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    cmdclass={'build_src': build_src},
     ext_modules=ext_modules,
     packages=packages,
     install_requires=["numpy", "scipy", "python-casacore"],
