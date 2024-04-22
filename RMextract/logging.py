1487cd157db17451bd597f42a59a91d72296a4ec
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2024-04-22 09:46:27 +0800

    Add loggin

---
+++ RMextract/logging.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Set up logging for RMextract
+"""
+import logging
+import os
+
+
+def setup_logging():
+    ctrl = os.environ.get("RMEXT_LOGLEVEL", "INFO")
+    if ctrl == "ERROR":
+        level = logging.ERROR
+    elif ctrl == "WARNING":
+        level = logging.WARNING
+    elif ctrl == "INFO":
+        level = logging.INFO
+
+    logger = logging.getLogger("RMextract")
+    logger.setLevel(level)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    ch.setLevel(level)
+    logger.addHandler(ch)
+    return logger
+
+logger = setup_logging()
